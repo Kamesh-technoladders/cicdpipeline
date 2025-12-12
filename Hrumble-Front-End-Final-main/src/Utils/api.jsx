34d44a3f5491a4b0fdb6aa8c9917e72a3913480f
Kamesh-technoladders <Kamesh.t@technoladders.com>
2025-12-12 15:22:32 +0530

    Update api.jsx

--- Hrumble-Front-End-Final-main/src/Utils/api.jsx
+++ Hrumble-Front-End-Final-main/src/Utils/api.jsx
@@ -1,6 +1,6 @@
 
-export const BASE_URL = `https://apiv1.technoladders.com/api/v1`;
-  export const BASE = `https://apiv1.technoladders.com/`;
+export const BASE_URL = `https://zive-backend.vercel.app/api/v1`;
+  export const BASE = `https://zive-backend.vercel.app/`;
 // export const BASE = `http://localhost:8080/`
 // export const BASE_URL = `http://localhost:8080/api/v1`
 
